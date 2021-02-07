74de3f52bcdfe065432f78cdf1e552cd4b870f72
Markus Altendorfer <markus.altendorfer@hotmail.de>
2021-02-07 13:22:00 +0100

    edit compose file

--- startprod.sh
+++ startprod.sh
@@ -5,7 +5,7 @@ docker-compose -f compose-prod.yml up -d
 
 read -p "Enter vault unseal key1: " vaultunseal1
 read -p "Enter vault unseal key2: " vaultunseal2
-read -p "Enter vault unseal key2: " vaultunseal3
+read -p "Enter vault unseal key3: " vaultunseal3
 
 docker exec -it vault bin/sh -c "vault operator unseal $vaultunseal1"
 docker exec -it vault bin/sh -c "vault operator unseal $vaultunseal2"
