ed800f340e6fbe7c924a65b8d98d80925219485b
Markus Altendorfer <markus.altendorfer@hotmail.de>
2021-02-07 18:48:07 +0100

    change

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -98,7 +98,7 @@ app.get('/api/admin/backendadmins', async (req:Request, res:Response, next:NextF
     
     const result = fetch('http://backend_api/api/admin/backendadmins', {header: { 'Authorization': `Bearer ${req.headers.authorization}`}});
     console.log(req.headers.authorization);
-    const body = await result.json();
+    const body = await result;
     res.status(200).json(body);
 });
 
