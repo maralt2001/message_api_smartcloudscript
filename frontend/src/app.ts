e8d32ec4f52837546105e15f903fb6976a18b32a
Markus Altendorfer <markus.altendorfer@hotmail.de>
2021-02-07 14:54:55 +0100

    change frontend_api

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -93,6 +93,13 @@ app.get('/api/airport', async (req:Request, res:Response, next:NextFunction) => 
     }
 })
 
+app.get('/api/admin/backendadmins', async (req:Request, res:Response, next:NextFunction) => {
+
+    const result = await fetch('http://backend_api/api/admin/backendadmins');
+    const body = await result.json();
+    res.status(200).json(body);
+});
+
 app.get('/api/admin/dbstatus', async (req:Request, res:Response, next:NextFunction) => {
 
     const result = await fetch('http://backend_api/api/admin/dbstatus');
