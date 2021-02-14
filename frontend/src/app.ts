1c6be54749d675e2d9464ff36f4f1201360e44bf
Markus Altendorfer <markus.altendorfer@hotmail.de>
2021-02-14 12:41:20 +0100

    add frontend backendadmins/count

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -89,6 +89,26 @@ app.get('/api/airport', async (req:Request, res:Response, next:NextFunction) => 
     } catch (error) {
         res.status(400).json({BadRequest: 'something went wrong'})
     }
+});
+
+app.get('/api/admin/backendadmins/count', async (req:Request, res:Response, next:NextFunction) => {
+
+    try {
+        const query = req.query;
+        var key = Object.keys(query).toString();
+        var value = Object.values(query).toString();
+        const result = await fetch(`http://backend_api/api/admin/backendadmins/count?${key}=${value}`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `${req.headers.authorization}`
+            }
+        });
+        const body = await result.json();
+        res.status(200).json(body);
+    } catch (error) {
+        res.status(400).json({BadRequest: 'something went wrong'});
+    }
 })
 
 app.get('/api/admin/backendadmins', async (req:Request, res:Response, next:NextFunction) => {
@@ -106,8 +126,6 @@ app.get('/api/admin/backendadmins', async (req:Request, res:Response, next:NextF
     } catch (error) {
         res.status(400).json({BadRequest: 'something went wrong'});
     }
-    
-    
 
 });
 
