997b0ee0c704184be7bfd5d71535c6b4a12a4be6
Markus Altendorfer <markus.altendorfer@hotmail.de>
2021-02-16 18:22:09 +0100

    change fetch

--- frontend/src/controller/backendMetricsRouter.ts
+++ frontend/src/controller/backendMetricsRouter.ts
@@ -2,6 +2,8 @@ var express = require('express')
 var router = express.Router()
 import {Request, Response, NextFunction} from 'express'
 
+const fetch = require('node-fetch');
+
 
 router.get('/', async(req:Request, res:Response, next:NextFunction) => {
     
